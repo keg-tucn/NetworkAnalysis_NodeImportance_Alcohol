c678ed2ac63528fa88ceedd6f730365bf42453b7
Mihai <mmahalean@montran.com>
2018-12-13 17:30:58 +0200

    Doing the SVMs

--- Graphs/Test.py
+++ Graphs/Test.py
@@ -10,7 +10,9 @@ proc=MatProc()
 #mat=proc.binarize(mat)
 #np.savetxt("test.csv",mat,delimiter=' , ')
 #size=mat.shape;
-matsi=reader.getReadingsByConditionAndAnimal(4,"Control")
+condition="Control"
+animalId=4
+matsi=reader.getReadingsByConditionAndAnimal(animalId,condition)
 index=0
 #np.savetxt("da" + str(index) + ".txt", matsi, delimiter=' ')
 print(matsi)
@@ -18,14 +20,29 @@ for matu in matsi:
     #np.savetxt("test.csv",matu,delimiter=' , ')
 
     aux=proc.binarize(matu)
-    np.savetxt("out"+str(index)+".txt",aux,delimiter=' ')
+   # np.savetxt("./tmp/out"+str(index)+".txt",aux,delimiter=' ')
     size=aux.shape
-    with open("graf"+str(index)+".txt","a") as file:
+    with open("./tmp/graf"+str(index)+".txt",'w+') as file:
         for i in range(0,size[1]):
             for j in range(i+1,size[0]):
                 if aux[i][j]==1:
                     file.write(str(i) + " " + str(j) + "\n");
-    os.system("node2vec_main.py"+ " --input graf"+str(index)+".txt"+ " --output embedding"+str(index)+".txt")
+    os.system("python ./node2vec_main.py"+" --input ./tmp/graf"+str(index)+".txt"+ " --output ./tmp/embedding"+condition+str(index)+".txt")
+    index=index+1
+condition="EtOH"
+alcohol=reader.getReadingsByConditionAndAnimal(animalId,condition)
+for matu in alcohol:
+    #np.savetxt("test.csv",matu,delimiter=' , ')
+
+    aux=proc.binarize(matu)
+   # np.savetxt("out"+str(index)+".txt",aux,delimiter=' ')
+    size=aux.shape
+    with open("./tmp/graf"+str(index)+".txt","w+") as file:
+        for i in range(0,size[1]):
+            for j in range(i+1,size[0]):
+                if aux[i][j]==1:
+                    file.write(str(i) + " " + str(j) + "\n");
+    os.system("python ./node2vec_main.py"+" --input ./tmp/graf"+str(index)+".txt"+ " --output ./tmp/embedding"+condition+str(index)+".txt")
     index=index+1
 
 
